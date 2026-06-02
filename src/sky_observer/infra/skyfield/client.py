02bb6c887e3825c886650dd265ec6ae768e196af
Gustavo Lisboa <130182272+yamatomate@users.noreply.github.com>
2026-06-01 22:04:06 -0300

    refactor: Refatorar o client do SkyField

--- src/sky_observer/infra/skyfield/client.py
+++ src/sky_observer/infra/skyfield/client.py
@@ -1,14 +1,25 @@
 from dataclasses import dataclass
-
 from skyfield import api, units
-from skyfield.api import N, W, load, wgs84
+from skyfield.api import N, W, Angle, load, wgs84
+
+@dataclass(frozen=True)
+class SearchObjectResponse:
+  visible: bool
+  altitude: Angle
+  azimuth: Angle
+
+@dataclass(frozen=True)
+class AllObjectResponse:
+  name: str
+  visible: bool
+  altitude: Angle
+  azimuth: Angle
 
 
 @dataclass(frozen=True)
-class ObjetoVisivelResponse:
-  visible: bool
-  altitude: float
-  azimuth: float
+class SearchObjectError:
+  error: int
+  message: str
 
 """
 Primeiro passe a lua localização logituginal e latituninal.
@@ -20,38 +31,51 @@ coisa que vc pode fazer:
 - ver que horas o planeta "nasce" e se "poe"
 """
 
+
 class SkyFieldClient:
-  def __init__(self, latitude: float, longitude: float):
-    """Classe que simplfica a busca dos objetos no ceu. Use a função objetos_observaveis para saber todos os objetos observaveis disponivveis para calculo.\n
-    posicao (tupla): recebe dois valores a latitude e a logitude\nexemplo: (5.3444, 2.5555)"""
-    self.posicao_atual: tuple[float, float] = (latitude,longitude)
+  posicao_atual : tuple[float, float]
+  escala_tempo : api.Timescale
+
+  def __init__(self, with_earth=False) -> None:
     self.escala_tempo = load.timescale()
 
     EPH = api.load("de421.bsp")
-    self.EPH = EPH
+    self._EPH = EPH
 
     self.TERRA = EPH["Earth"]
-    planetas = {
-      "Mercúrio": EPH["Mercury"],
-      "Vênus": EPH["Venus"],
-      "Terra": EPH["Earth"],
-      "Marte": EPH["Mars"],
-      "Júpiter": EPH[5],
-      "Saturno": EPH[6],
-      "Urano": EPH[7],
-      "Netuno": EPH[8],
-    }
-    self.planetas = planetas
+
+    if with_earth:
+      self.planetas = {
+        "Mercúrio": EPH["Mercury"],
+        "Vênus": EPH["Venus"],
+        "Terra": EPH["Earth"],
+        "Marte": EPH["Mars"],
+        "Júpiter": EPH[5],
+        "Saturno": EPH[6],
+        "Urano": EPH[7],
+        "Netuno": EPH[8],
+      }
+    else:
+      self.planetas = {
+        "Mercúrio": EPH["Mercury"],
+        "Vênus": EPH["Venus"],
+        "Marte": EPH["Mars"],
+        "Júpiter": EPH[5],
+        "Saturno": EPH[6],
+        "Urano": EPH[7],
+        "Netuno": EPH[8],
+      }
+    
     self.satelites = {"Lua": EPH["Moon"]}
     self.extra = {"Plutão": EPH[9]}
-
     self.observaveis = self.planetas | self.satelites | self.extra
+
+  def set_location(self, latitude: float = 0.0, logitude: float = 0.0):
+    self.posicao_atual = (latitude, logitude)
 
   def search_object(
     self,
     objeto="Moon",
-    latitude: float = None,
-    longitude: float = None,
     horario=load.timescale().now(),
   ):
     """Verificar se no na hora passada se o objeto está visivel no ceu da terra.\n
@@ -60,26 +84,55 @@ class SkyFieldClient:
     horario: por padrão será usado o atual"""
 
     alvo = None
-    if latitude is None and longitude is None:
-      latitude = self.posicao_atual[0]
-      longitude = self.posicao_atual[1]
 
     if objeto in self.observaveis:
       alvo = self.observaveis[objeto]
-    elif objeto in self.EPH and objeto != 0:
-      alvo = self.EPH[objeto]
-    else:
+    elif objeto in self._EPH and objeto != 0:
+      alvo = self._EPH[objeto]
+    elif self.posicao_atual == None:
       # alvo não foi nem encotrado na lista de observaveis e nem na EPH
-      return None
+      return SearchObjectError(error=1, message="Posicao atual não definida")
+    else:
+      return SearchObjectError(error=2, message="Objeto não encontrado")
+
+    latitude = self.posicao_atual[0]
+    longitude = self.posicao_atual[1]
 
     local_observacao = self.TERRA + wgs84.latlon(latitude * N, longitude * W)
     astrometric = local_observacao.at(horario).observe(alvo)
-    alt, az, d = astrometric.apparent().altaz()
-    vis = alt.degrees > units.Angle(degrees=1).degrees
-    return ObjetoVisivelResponse(vis, alt, az)
+    alti, azi, dis = astrometric.apparent().altaz()
+    vis = alti.degrees > units.Angle(degrees=1).degrees
+
+    return SearchObjectResponse(visible=vis, altitude=alti, azimuth=azi)
+
+  def list_all_observable_objects(self, horario : load.timescale = load.timescale().now()):
+    if self.posicao_atual == None:
+      # alvo não foi nem encotrado na lista de observaveis e nem na EPH
+      return SearchObjectError(error=1, message="Posicao atual não definida")
+
+    latitude = self.posicao_atual[0]
+    longitude = self.posicao_atual[1]
+
+    local_observacao = self.TERRA + wgs84.latlon(latitude * N, longitude * W)
+
+    objetos = []
+    for key, value in self.observaveis.items():
+      print(key)
+      astrometric = local_observacao.at(horario).observe(value)
+      alti, azi, dis = astrometric.apparent().altaz()
+      vis = alti.degrees > units.Angle(degrees=1).degrees
+      objetos.append(AllObjectResponse(name=key,visible=vis, altitude=alti, azimuth=azi))
+
+    print(objetos)
 
   def observable_objects(self, modo: int = 1):
     if modo == 1:
       return [chaves for chaves in self.observaveis]
     elif modo == 2:
-      return self.EPH
+      return self._EPH
+
+if __name__ == "__main__":
+  client = SkyFieldClient()
+  client.set_location(latitude=0,logitude=0)
+  print(client.search_object(objeto="Moon"))
+  print(client.list_all_observable_objects())
---
+++ test/infra/skyfiled/client_test.py
@@ -0,0 +1,5 @@
+from logging import getLogger
+from unittest.mock import MagicMock, patch
+
+from sky_observer.infra.skyfield.client import SkyFieldClient, ObjetoVisivelResponse
+
